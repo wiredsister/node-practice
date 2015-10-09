080fc89fdd33e803acf5838ad64b9fe307eae913
Gina Maini <drawginadraw@gmail.com>
2015-10-08 17:33:28 -0700

    issues with Edgars build, so commiting my build file for demo

---
+++ build/Release/.deps/Release/fourtytwo.node.d
@@ -0,0 +1,1 @@
+cmd_Release/fourtytwo.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/fourtytwo.node Release/obj.target/fourtytwo/fourtytwo.o 
---
+++ build/Release/.deps/Release/obj.target/fourtytwo/fourtytwo.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/fourtytwo/fourtytwo.o := c++ '-DNODE_GYP_MODULE_NAME=fourtytwo' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/gina/.node-gyp/4.1.2/include/node -I/Users/gina/.node-gyp/4.1.2/src -I/Users/gina/.node-gyp/4.1.2/deps/uv/include -I/Users/gina/.node-gyp/4.1.2/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/fourtytwo/fourtytwo.o.d.raw  -c -o Release/obj.target/fourtytwo/fourtytwo.o ../fourtytwo.cpp
+Release/obj.target/fourtytwo/fourtytwo.o: ../fourtytwo.cpp \
+  /Users/gina/.node-gyp/4.1.2/include/node/node.h \
+  /Users/gina/.node-gyp/4.1.2/include/node/v8.h \
+  /Users/gina/.node-gyp/4.1.2/include/node/v8-version.h \
+  /Users/gina/.node-gyp/4.1.2/include/node/v8config.h \
+  /Users/gina/.node-gyp/4.1.2/include/node/node_version.h
+../fourtytwo.cpp:
+/Users/gina/.node-gyp/4.1.2/include/node/node.h:
+/Users/gina/.node-gyp/4.1.2/include/node/v8.h:
+/Users/gina/.node-gyp/4.1.2/include/node/v8-version.h:
+/Users/gina/.node-gyp/4.1.2/include/node/v8config.h:
+/Users/gina/.node-gyp/4.1.2/include/node/node_version.h:
